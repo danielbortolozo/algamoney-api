8172268e1f245d8c2f65d7a89606e827929b4876
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-02-17 23:22:46 -0300

    Criação de handlerException

--- src/main/java/com/sisdb/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/sisdb/algamoney/api/resource/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -41,10 +42,12 @@ public class CategoriaResource {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria entity, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria entity, HttpServletResponse response) {
+		
 		Categoria categoriaSalva = categoriaRepository.save(entity);
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-		.buildAndExpand(categoriaSalva.getCodigo()).toUri();		
+		          .buildAndExpand(categoriaSalva.getCodigo()).toUri();			
 		response.setHeader("Location", uri.toASCIIString());
 		
 		return ResponseEntity.created(uri).body(categoriaSalva);
